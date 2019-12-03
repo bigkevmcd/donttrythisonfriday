9f1b9ec3b4029396728804fc48a8346698890b89
Kevin McDermott <kmcdermo@redhat.com>
2019-12-03 09:26:52 +0000

    Add support for replacing the GitHub repo.

--- setup.sh
+++ setup.sh
@@ -1,15 +1,26 @@
 #!/bin/sh
 QUAYIO_USERNAME=$1
-APPNAME=$2
+GITHUB_REPO=$2
 
+if [[ $# -ne 2 ]]; then
+    echo 'usage: ./setup.sh <quayio-username> <github repo>'
+    exit 1
+fi
+
+IFS='/' # assumes orgname/repo
+read -ra seg <<< "${GITHUB_REPO}"
+
+if [[ ${#seg[@]} -ne 2 ]]; then
+  echo 'github repo must be of the form orgname/repo'
+fi
+
+ORGNAME=${seg[0]}
+APPNAME=${seg[0]}
 PULL_SECRET_NAME="${QUAYIO_USERNAME}-secret"
 DOCKER_CONFIG_NAME="${QUAYIO_USERNAME}-auth.json"
 IMAGE_REPO="quay.io/${QUAYIO_USERNAME}/${APPNAME}"
-
-if [[ $# -ne 2 ]] ; then
-    echo 'usage: ./setup.sh <quayio-username> <appname>'
-    exit 1
-fi
+GITHUB_REPO="${ORGNAME}/${APPNAME}"
+GITHUB_STAGE_REPO="${ORGNAME}/${APPNAME}-stage-config"
 
 FILENAME="$HOME/Downloads/${DOCKER_CONFIG_NAME}"
 if [ ! -f "${FILENAME}" ]; then
@@ -26,3 +37,5 @@ fi
 sed -i "s|REPLACE_IMAGE|${IMAGE_REPO}|g" deploy/**/*.yaml pipelines/**/*.yaml
 sed -i "s|PULL_SECRET_NAME|${PULL_SECRET_NAME}|g" pipelines/serviceaccount/serviceaccount.yaml pipelines/bootstrap.sh
 sed -i "s|DOCKER_CONFIG_NAME|${DOCKER_CONFIG_NAME}|g" pipelines/bootstrap.sh
+sed -i "s|GITHUB_REPO|${GITHUB_REPO}|g" pipelines/eventlisteners/cicd-event-listener.yaml
+sed -i "s|GITHUB_STAGE_REPO|${GITHUB_STAGE_REPO}|g" pipelines/eventlisteners/cicd-event-listener.yaml
