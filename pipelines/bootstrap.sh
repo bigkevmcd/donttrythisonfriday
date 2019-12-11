d8a8ee6c0517826bb95369dc1a0bf77adf351cc3
Kevin McDermott <kmcdermo@redhat.com>
2019-12-11 10:37:05 +0000

    Manual setup for ArgoCD.

--- pipelines/bootstrap.sh
+++ pipelines/bootstrap.sh
@@ -19,5 +19,9 @@ oc apply -f ci
 oc apply -f cd
 oc apply -f eventlisteners
 oc apply -f routes
-oc apply -f argocd
 oc create secret generic github-auth --from-file=$HOME/Downloads/token
+PASSWORD=$(kubectl get pods -n argocd -l app.kubernetes.io/name=argocd-server -o name | cut -d'/' -f 2)
+echo "You will need to login to the ArgoCD server argocd login <SERVER> with username admin and password $(PASSWORD)"
+echo "and then you _MUST_ change the password with argocd account update-password"
+echo "Finally, you need to create the application:"
+echo "  argocd app create -f argocd/argo-app.yaml"
